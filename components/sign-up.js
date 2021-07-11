bf146e3b35c14b9b4a729b250e119fee6a7f5b3c
rsp243 <reshetov.semjon@gmail.com>
2021-07-12 01:12:57 +0400

    registration continue button fixed

--- components/sign-up.js
+++ components/sign-up.js
@@ -25,6 +25,15 @@ app.component('sign-up-display', {
         <div class="btn-shell">
             <button class="sign-up-btn">
                 Продолжить
+                <svg width="24" height="28" viewBox="0 0 24 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24 14L2.26338e-06 27.8564L1.93291e-06 0.143594L24 14Z" fill="url(#paint2_linear)"/>
+                    <defs>
+                    <linearGradient id="paint2_linear" x1="30.3333" y1="37" x2="-10.3333" y2="-10.6667" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#4D186F"/>
+                        <stop offset="1" stop-color="#B073D5"/>
+                    </linearGradient>
+                    </defs>
+                </svg>
             </button>
         </div>
     </div>
